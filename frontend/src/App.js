1f15f564deaaa53045437b6cf2469ce45cb99f55
Mohamed <mohamedhema2017@gmail.com>
2023-05-14 23:27:10 +0300

    Starting FrontEnd

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import SignupPage from './pages/SignUp';
+
+function App() {
+    return (
+
+    );
+  }
+  
---
+++ frontend/src/pages/TeacherPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeacherPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TeacherPage
